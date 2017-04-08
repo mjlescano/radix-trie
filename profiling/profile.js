26e1dc7a69ef2bbf4a8cfeeca3b2e9e9c5a69c23
Facu Velasco <velascofacundo@gmail.com>
2017-04-08 16:02:02 -0300

    remove test to profile

--- profiling/profile.js
+++ profiling/profile.js
@@ -10,13 +10,36 @@ const tb = Date.now() - t0;
 console.log(`Building Index for ${number} items: ${tb} ms`);
 
 const t1 = Date.now();
-console.log(words.every(function(word) {
-  if(!trie.findWord(word)){
+console.log('Find all:', words.every(function(word) {
+  if(!trie.findNode(word)){
     return false
   }
   return true
 }));
 const ts = Date.now() - t1;
 console.log(`Searching ${number} items: ${ts} ms`);
+
+let aux = 0;
+const t2 = Date.now();
+words.map((word) => {
+  if(word.length%3 === 0){
+    aux += 1;
+    trie.removeWord(word,'test');
+  }
+})
+const tr = Date.now() - t2;
+console.log(`Removing ${aux} items: ${tr} ms`);
+
+const t3 = Date.now();
+console.log('Find all:', words.every(function(word) {
+  if(word.length%3 !== 0){
+    if(!trie.findNode(word)){
+      return false
+    }
+  }
+  return true
+}));
+const tf = Date.now() - t3;
+console.log(`Searching ${number - aux} items after remove: ${tf} ms`);
 
 process.exit()
